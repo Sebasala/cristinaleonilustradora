746ba49005fc703ba670ec05bfdc03122aba3538
Sebastian Salazar Ramirez <sebsalaz2@WKMZTB39D604.global.publicisgroupe.net>
2024-01-05 15:29:02 -0500

    Alt attributes

---
+++ src/data/images.js
@@ -0,0 +1,48 @@
+import img1 from '@assets/img1.jpg';
+import img2 from '@assets/img2.jpg';
+import img3 from '@assets/img3.jpg';
+import img4 from '@assets/img4.jpg';
+import img5 from '@assets/img5.jpg';
+import img6 from '@assets/img6.jpg';
+import img7 from '@assets/img7.jpg';
+import img8 from '@assets/img8.jpg';
+import img9 from '@assets/img9.jpg';
+const images = [
+	{
+		src: img1,
+		alt: 'Imagen 1',
+	},
+	{
+		src: img2,
+		alt: 'Imagen 2',
+	},
+	{
+		src: img3,
+		alt: 'Imagen 3',
+	},
+	{
+		src: img4,
+		alt: 'Imagen 4',
+	},
+	{
+		src: img5,
+		alt: 'Imagen 5',
+	},
+	{
+		src: img6,
+		alt: 'Imagen 6',
+	},
+	{
+		src: img7,
+		alt: 'Imagen 7',
+	},
+	{
+		src: img8,
+		alt: 'Imagen 8',
+	},
+	{
+		src: img9,
+		alt: 'Imagen 9',
+	},
+]
+export default images;
