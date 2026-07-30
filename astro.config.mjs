30841d89edaefe7c0a4068af84020d68a75ea799
Sebastián Salazar <6620671+Sebasala@users.noreply.github.com>
2026-07-30 17:29:28 -0500

    Images optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cristinaleonilustradora.com/'
+  site: "https://cristinaleonilustradora.com/",
+  image: {
+    responsiveStyles: true,
+    layout: "constrained"
+  }
 });
