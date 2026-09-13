d1857913e79c5641427afcacec807d4a539a3c51
Sebastián Salazar <6620671+Sebasala@users.noreply.github.com>
2026-09-13 15:32:07 -0500

    Migracion de cristinaleonilustradora.com a cristinaleon.art

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cristinaleonilustradora.com/",
+  site: "https://cristinaleon.art",
 
   image: {
     responsiveStyles: true,
     layout: "constrained"
   },
 
-  integrations: [mdx()]
+  integrations: [mdx(), sitemap()]
 });
